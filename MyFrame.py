0d012db1cb7a70c6c639305af01daed1e4d703b1
zjzhai <zjzhai@gmail.com>
2011-10-28 09:58:54 +0800

    进行优化

--- MyFrame.py
+++ MyFrame.py
@@ -14,6 +14,7 @@ class iFrame(wx.Frame):
         super(iFrame, self).__init__(parent, title = title,
             size = (455, 130), style = wx.DEFAULT_FRAME_STYLE ^ wx.MAXIMIZE_BOX ^ wx.RESIZE_BORDER)
         self.dirAddress = ''
+        self.unknow = ' \r\n unknow \r\n ------------ \r\n'
         self.mingDan = ''
         self.listStu = {}
         self.InitUI()
@@ -32,57 +33,68 @@ class iFrame(wx.Frame):
         
         hbox2 = wx.BoxSizer(wx.HORIZONTAL)
         startBtn = wx.Button(panel, -1, label = "Start")
+        self.Bind(wx.EVT_BUTTON, self.startBtnE, startBtn)
+        
+        openBtn = wx.Button(panel, -1, label = "Open")
+        self.Bind(wx.EVT_BUTTON, self.openBtnE, openBtn)
+        
         hbox2.Add(startBtn,flag = wx.EXPAND|wx.RIGHT)
+        hbox2.Add(openBtn,flag = wx.EXPAND|wx.RIGHT)
         contrainBox.Add(hbox2, flag = wx.ALIGN_RIGHT|wx.TOP, border = 10)
         panel.SetSizer(contrainBox)
-        self.Bind(wx.EVT_BUTTON, self.btnE, startBtn)
         
-        #self.createMenuBar()
+   
+    def calculate(self, address):
+        self.listStu = self.readListOfStudent(self.mingDan)
+        self.loopIt(address)
+        self.writeMingDan(self.mingDan, self.convert_disc_string(self.listStu)+ self.unknow)
+        wx.MessageDialog(None, 'finished!', " ", wx.OK).ShowModal()
+        self.openWithNotepad(self.mingDan)
         
-
     def createMenuBar(self):
         menuBar = wx.MenuBar()
         helpMenu = wx.Menu()
-        #helpItem = helpMenu.Append(wx.ID_ANY, u"使用说明", '' )
         menuBar.Append(helpMenu, '&Help')
         self.SetMenuBar(menuBar)
-        #selfBind(wx.EVT_MENU, self.helpMenuCallback, helpMenu)
 
     def helpMenuCallback(self, e):
         pass
     
     def dbbCallback(self, e):
-        #wx.MessageDialog(None, e.GetString().strip()," ", wx.OK).ShowModal()
         if not os.path.isdir(e.GetString().strip()):
-            wx.MessageDialog(None, u"必须是目录"," ", wx.OK).ShowModal()
+            wx.MessageDialog(None, u"必须是目录", " ", wx.OK).ShowModal()
             e.SetValue(self,'')
         else :
             self.dirAddress = e.GetString().strip()
+            self.mingDan = self.returnMingDan(self.dirAddress)
         
 
-    def btnE(self,e):
-        self.mingDan = os.path.join(self.dirAddress, self.dirAddress.split(os.path.sep)[-1] + '.txt')
-       
-        if not os.path.exists(self.mingDan):
-            wx.MessageDialog(None, u'名单不存在', u"注意", wx.YES_NO).ShowModal()
-            os.system('cd ' + self.dirAddress.encode(codingUTF))
-            return
-        if not self.dirAddress :
-            wx.MessageDialog(None, u'地址不能为空',u"注意", wx.YES_NO).ShowModal()
+    def startBtnE(self,e):
+        if not self.checkFileAndAlert(self.mingDan) :
             return
         else :
             self.calculate(self.dirAddress)
+
+    #打开名单   
+    def openBtnE(self, e):
+        if self.checkFileAndAlert(self.mingDan):
+            pass
+        else:
+            self.openWithNotepad(self.mingDan)
+
+    #使用notepad打开
+    def openWithNotepad(self, address):
+        os.system('notepad ' + address.encode(codingGBK))
+
+    #名单不存在，则警告
+    def checkFileAndAlert(self, fileAddress):
+        if not os.path.exists(fileAddress):
+            wx.MessageDialog(None, u'名单不存在', u"注意", wx.OK).ShowModal()
+            return False
+        else:
+            return True
             
-                
-    
-    def calculate(self, address):
-        
-        self.listStu = self.readListOfStudent(self.mingDan)
-        self.loopIt(address)
-        self.writeMingDan(self.mingDan, self.convert_disc_string(self.listStu))
-        wx.MessageDialog(None, 'finished!', " ", wx.OK).ShowModal()
-        os.system('notepad ' + self.mingDan)
-        
+
     #读取名单进入字典中
     def readListOfStudent(self, md):
         listStu = {}
@@ -96,8 +108,11 @@ class iFrame(wx.Frame):
     #遍历文件并统计分数,写入字典
     def loopIt(self, address):
         for dirpath, dirnames, filenames in os.walk(address):
+           
             baseName = self.pullChinese(os.path.basename(dirpath).strip())
-            #是否给学生加分规则
+            #是否给学生加分的规则
+            if len(baseName) == 0:
+                self.unknow += dirpath.strip()+'\r\n'
             if self.listStu.has_key(baseName) and filenames:
                 self.listStu[baseName] += 1
     
@@ -115,7 +130,7 @@ class iFrame(wx.Frame):
         return txt
     
     #抽取中文
-    def pullChinese(self,txt):
+    def pullChinese(self, txt):
         ret_str = []
         for s in txt:
             if self.is_cn_char(s):
@@ -125,6 +140,14 @@ class iFrame(wx.Frame):
 
     def is_cn_char(self, i): 
         return 0x4e00 <= ord(i) < 0x9fa6
+
+    #检测名单路径是否合法
+    def checkMingDanAndDialog():
+        pass
+
+    #返回名单
+    def returnMingDan(self, address):
+         return os.path.join(address, address.split(os.path.sep)[-1] + '.txt')
     
 if __name__ == '__main__':
     app = wx.App()
